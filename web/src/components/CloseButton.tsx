b7da56155c6130c38bda87ac7b5c6291e50684c3
Flavio Rocha <fmrocha@gmail.com>
2022-05-04 16:20:33 -0400

    Feat. Forms Bugs Idea Others

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -3,7 +3,7 @@ import { X } from "phosphor-react";
 
 export function CloseButton() {
   return (
-    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Close Feedback form">
+    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100 border-transparent" title="Close Feedback form ">
       <X weight="bold" className="w-4 h-4 "/>
     </Popover.Button>
   )
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeebackSuccess";
 
 export const feedbackTypes = {
   BUG: {
@@ -34,20 +35,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
-  
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestarFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Feedback here</span>
-
-        <CloseButton />
-      </header>
-
-      { !feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-      ): (
-        <p>Hello World!</p>
-      )}
+      { feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestarFeedback}/>
+      ) : (
+        <>
+          { !feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+          ): (
+            <FeedbackContentStep 
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestarFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )
+    }
       
       <footer>
         Created by <a className="underline underline-offset-2" href="https://www.linkedin.com/in/fmrocha/">Flavio Rocha</a>
